2bb40d0d3dc1481767669f979ccd5e71c62ff598
Derek Hampton <derek.hampton@augmensis.co.uk>
2014-06-05 14:36:50 +0000

    fixed sign in to lowercase email validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(:email => params[:email]).first
+    user = User.where(:email => params[:email].downcase).first
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :guesses
+  
+  before_save { |user| user.email = email.downcase }
 
   validates :email, uniqueness: true, length: { maximum: 40 }
   validates_format_of :email, with: %r{\A[A-Za-z0-9._%+-]+@(?:[A-Za-z0-9.-]+\.)+[A-Za-z]+\z}
